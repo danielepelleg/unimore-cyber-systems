55b97228d6f4878361abb5e13b9e711b6674fb83
danielepelleg <daniele.pellegrini@studenti.unipr.it>
2023-05-10 20:31:58 +0200

    Move read() function in pgm.cpp

--- Multimedia Data Processing/Laboratorio/08 - PGM Format/main.cpp
+++ Multimedia Data Processing/Laboratorio/08 - PGM Format/main.cpp
@@ -18,59 +18,9 @@ auto make_test_pattern() {
 	return img;
 }
 
-auto read(const std::string &filename) {
-	matrix<uint8_t> img;
-	std::ifstream is(filename, std::ios::binary);
-	if (!is) {
-		return img;
-	}
-
-	std::string magic_number;
-	std::getline(is, magic_number);
-	pgm_mode mode;
-	if (magic_number != "P2") {
-		mode = pgm_mode::plain;
-	}
-	else if (magic_number == "P5") {
-		mode = pgm_mode::binary;
-	}
-	else {
-		return img;
-	}
-
-	if (is.peek() == '#') {
-		std::string comment;
-		std::getline(is, comment);
-	}
-
-	int width, height, nlevels;
-	char newline;
-	is >> width >> height >> nlevels;
-	is.get(newline);
-
-	if (nlevels != 255 || newline != '\n') {
-		return img;
-	}
-	img.resize(height, width);
-
-	if (mode == pgm_mode::plain) {
-		for (auto &x : img) {
-			int value;
-			// put an x here results in reading a single character
-			is >> value;
-			x = value;
-		}
-	}
-	else{
-		is.read(img.rawdata(), img.rawsize());
-	}
-
-	return img;
-}
-
 auto flip(matrix<uint8_t> &img) {
 	// flip the first half rows of the image with the bottom half
-	for (int r = 0; r < img.rows(); ++r) {
+	for (int r = 0; r < img.rows()/2; ++r) {
 		for (int c = 0; c < img.cols(); ++c) {
 			std::swap(img(r, c), img(img.rows() - 1 - r, c));
 		}
--- Multimedia Data Processing/Laboratorio/08 - PGM Format/pgm.h
+++ Multimedia Data Processing/Laboratorio/08 - PGM Format/pgm.h
@@ -7,3 +7,5 @@ enum pgm_mode { plain = 2, binary = 5 };
 // must be a 8 bit image beacause PGM
 // only support grey images
 bool write(const std::string &filename, const matrix<uint8_t> &img, pgm_mode mode);
+
+auto read(const std::string &filename);
